92a7386860347b1bde99c2333ce1b78d48297489
tahha00 <tahha2k9@gmail.com>
2024-10-30 15:31:09 +0000

    remove gitignore

--- SpecFlowExercise/obj/Debug/net6.0/SpecFlowExercise.AssemblyInfo.cs
+++ SpecFlowExercise/obj/Debug/net6.0/SpecFlowExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecFlowExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b803949fbe590b3557b4a779f33ee1673191b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecFlowExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpecFlowExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
